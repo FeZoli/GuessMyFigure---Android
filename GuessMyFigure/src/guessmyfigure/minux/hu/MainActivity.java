f4e9d7b613dac6b195ff20ce3a252cc427150979
Fekete, Zoltán <zfekete74@gmail.com>
2013-11-07 13:49:13 +0100

    ActionBar structure has been finished.

--- GuessMyFigure/src/guessmyfigure/minux/hu/MainActivity.java
+++ GuessMyFigure/src/guessmyfigure/minux/hu/MainActivity.java
@@ -2,11 +2,13 @@ package guessmyfigure.minux.hu;
 
 import java.util.Random;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,61 +16,40 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    private int myGuess;
-    private Button tipButton;
-    private ActionBar actionBar;
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);      
-        createFigureToBeGuessed();
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    
-    public void sendTip(View view) {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_quit:
+			this.finish();
+			return true;
+		case R.id.action_guesser:
+			startGuesser();
+			return true;
+		case R.id.action_rounding:
+			startRounding();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-        EditText tipText = (EditText) findViewById(R.id.edit_message);
-        TextView msgView = (TextView) findViewById(R.id.view_msg);
-        tipButton = (Button) findViewById(R.id.TipButton);
-        
-        if (tipText.getText().length() == 0  ) {
-        	return;
-        }
+	private void startGuesser() {
 
-        int userGuess = Integer.parseInt(tipText.getText().toString());
-        
-        if (userGuess < myGuess) {
-            msgView.setText(userGuess + " kisebb, mint amire gondoltam.");
-        } else if (userGuess > myGuess) {
-            msgView.setText(userGuess + " nagyobb, mint amire gondoltam.");
-        } else {
-        	tipButton.setEnabled(false);
-            msgView.setText(userGuess + ". Igen. KITALÁLTAD!!! Ügyes vagy!");
-        }
+		Intent intent = new Intent(this, GuesserActivity.class);
+		startActivity(intent);
 
-        tipText.setText("", TextView.BufferType.NORMAL);
-    }
+		return;
+	}
 
-    public void newGuess(View view) {
-        createFigureToBeGuessed();
-        TextView msgView = (TextView) findViewById(R.id.view_msg);
-        msgView.setText("Megvan, gondoltam egy újat. Próbálkozz!");
-        tipButton.setEnabled(true);
-    }
-
-    private void createFigureToBeGuessed() {
-
-        Random randomGenerator = new Random();
-        myGuess = randomGenerator.nextInt(100);
-
-    }
+	private void startRounding() {
+		return;
+	}
 }
